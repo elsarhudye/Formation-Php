dc634532467f1b76cedfc5ae78230d4b8f8346be
Elsa Rhudye Mangoye <diamant.rhudye@gmail.com>
2021-05-31 23:44:29 +0200

    remove echo in php

--- coursPhp/amazin.php
+++ coursPhp/amazin.php
@@ -26,96 +26,91 @@
         <!-- step 0 : A afficher uniquement au chargement de la page -->
         <?php 
             if(($_GET == null) || $_GET['question'] == 0){ 
-                echo '
-            <p>Vous avez contacté notre service client et nous aimerions avoir votre avis sur la qualité de ce service</p>
-            <p>Commencez le questionnaire : <a href="/amazin.php?question=1&result=0" role="button" class="btn btn-success">Commencer</a></p>
-            ';
+        ?>     
+        <p>Vous avez contacté notre service client et nous aimerions avoir votre avis sur la qualité de ce service</p>
+        <p>Commencez le questionnaire : <a href="/amazin.php?question=1&result=0" role="button" class="btn btn-success">Commencer</a></p>
+        <?php 
             }
         ?>
         
         
-            <!-- Etape 1 : A afficher uniquement une fois que le boutton "Commencer" a été pressé -->
-         <?php 
+        <!-- Etape 1 : A afficher uniquement une fois que le boutton "Commencer" a été pressé -->
+        <?php 
             if(isset($_GET['question']) && $_GET['question'] == 1){ 
-                echo '
+        ?>  
             <h2>Question 1</h2>
-            <p>L\'agent a-t-il été agréable ?</p>
-
-            <a href="?question=2&result='.($_GET['result'] + 2).'" role="button" class="btn btn-success">oui</a> <!-- rapporte 2 point -->
-            <a href="?question=2&result='.($_GET['result'] + 0).'" role="button" class="btn btn-danger">non</a> <!-- rapporte 0 point -->
-            <a href="?question=2&result='.($_GET['result'] + 1).'" role="button" class="btn btn-secondary">sans avis</a> <!-- rapporte 1 point -->
-            ';
+            <p>L'agent a-t-il été agréable ?</p>
+            <a href="?question=2&result=<?= $_GET['result'] + 2 ?>" role="button" class="btn btn-success">oui</a> <!-- rapporte 2 point -->
+            <a href="?question=2&result=<?= $_GET['result'] + 0 ?>" role="button" class="btn btn-danger">non</a> <!-- rapporte 0 point -->
+            <a href="?question=2&result=<?= $_GET['result'] + 1 ?>" role="button" class="btn btn-secondary">sans avis</a> <!-- rapporte 1 point -->
+        <?php 
             }
         ?>
         
         <!-- Etape 2 : A afficher uniquement une fois que l\'étape 1 a été résolue -->
         <?php 
-            if(isset($_GET['question']) && $_GET['question'] == 2){ 
-                echo '
+            if(isset($_GET['question']) && $_GET['question'] == 2){  
+        ?>  
             <h2>Question 2</h2>
-            <p>L\'agent a-t-il compris votre problème ?</p>
-            <a href="?question=3&result='.($_GET['result'] + 2).'" role="button" class="btn btn-success">oui</a> <!-- rapporte 2 point -->
-            <a href="?question=3&result='.($_GET['result'] + 0).'" role="button" class="btn btn-danger">non</a> <!-- rapporte 0 point -->
-            <a href="?question=3&result='.($_GET['result'] + 1).'" role="button" class="btn btn-secondary">sans avis</a> <!-- rapporte 0 point -->
-            ';
+            <p>L'agent a-t-il compris votre problème ?</p>
+            <a href="?question=3&result=<?= $_GET['result'] + 2 ?>" role="button" class="btn btn-success">oui</a> <!-- rapporte 2 point -->
+            <a href="?question=3&result=<?= $_GET['result'] + 0 ?>" role="button" class="btn btn-danger">non</a> <!-- rapporte 0 point -->
+            <a href="?question=3&result=<?= $_GET['result'] + 1 ?>" role="button" class="btn btn-secondary">sans avis</a> <!-- rapporte 0 point -->
+        <?php 
             }
         ?>
         
         
         <!-- Etape 3 : A afficher uniquement une fois que l\'étape 2 a été résolue -->
         <?php 
-            if(isset($_GET['question']) && $_GET['question'] == 3){ 
-                echo '
+            if(isset($_GET['question']) && $_GET['question'] == 3){  
+        ?>  
             <h2>Question 3</h2>
-            <p>L\'agent a-t-il résolu votre problème ?</p>
-            <a href="?question=5&result='.($_GET['result'] + 2).'" role="button" class="btn btn-success">oui</a> <!-- rapporte 1 point -->
-            <a href="?question=4&result='.($_GET['result'] -1).'" role="button" class="btn btn-danger">non</a> <!-- rapporte -1 point -->
-                ';
+            <p>L'agent a-t-il résolu votre problème ?</p>
+            <a href="?question=5&result=<?= $_GET['result'] + 2 ?>" role="button" class="btn btn-success">oui</a> <!-- rapporte 1 point -->
+            <a href="?question=4&result=<?= $_GET['result'] -1 ?>" role="button" class="btn btn-danger">non</a> <!-- rapporte -1 point -->
+        <?php 
             }
         ?>
          
             
-        
         <!-- Etape 4 : A afficher uniquement si "non" a été répondu à l'étape 3 -->
         <?php 
             $numero = "0711111111";
-            if(isset($_GET['question']) && $_GET['question'] == 4){ 
-                echo '
-            <p>Votre problème n\'a pas été résolu.</p>
-            <p>Pour être rappelé, entrez votre numéro de téléphone dans le clavier virtuel et validez :</p>
-            <!-- Coder ici un clavier numérique permettant de saisir le numéro de téléphone -->
-            <a href="" role="button" class="btn btn-secondary">1</a> 
-            <a href="" role="button" class="btn btn-secondary">2</a> 
-            <a href="" role="button" class="btn btn-secondary">3</a> 
-            <a href="" role="button" class="btn btn-secondary">4</a> 
-            <a href="" role="button" class="btn btn-secondary">5</a> 
-            <br>
-            <a href="" role="button" class="btn btn-secondary">6</a> 
-            <a href="" role="button" class="btn btn-secondary">7</a> 
-            <a href="" role="button" class="btn btn-secondary">8</a> 
-            <a href="" role="button" class="btn btn-secondary">9</a> 
-            <a href="" role="button" class="btn btn-secondary">0</a> 
-            <br>
+            if(isset($_GET['question']) && $_GET['question'] == 4){   
+        ?>  
+        <p>Votre problème n'a pas été résolu.</p>
+        <p>Pour être rappelé, entrez votre numéro de téléphone dans le clavier virtuel et validez :</p>
+        <!-- Coder ici un clavier numérique permettant de saisir le numéro de téléphone -->
+        <a href="" role="button" class="btn btn-secondary">1</a> 
+        <a href="" role="button" class="btn btn-secondary">2</a> 
+        <a href="" role="button" class="btn btn-secondary">3</a> 
+        <a href="" role="button" class="btn btn-secondary">4</a> 
+        <a href="" role="button" class="btn btn-secondary">5</a> 
+        <br>
+        <a href="" role="button" class="btn btn-secondary">6</a> 
+        <a href="" role="button" class="btn btn-secondary">7</a> 
+        <a href="" role="button" class="btn btn-secondary">8</a> 
+        <a href="" role="button" class="btn btn-secondary">9</a> 
+        <a href="" role="button" class="btn btn-secondary">0</a> 
+        <br>
 
 
-            <!-- Afficher ici le numéro de téléphone qui s\'affiche au fur et à mesure de la saisie-->
+        <!-- Afficher ici le numéro de téléphone qui s\'affiche au fur et à mesure de la saisie-->
 
-            <p>Votre numéro : '.$numero.'</p>
-
-            <a href="?question=5&result='.($_GET['result']).'" role="button" class="btn btn-success">Valider</a> <!-- Validation du numéro de téléphone -->
-            
-
-
-            <!-- Mes boutons-->
-            <br><br>
-            <a href="?question=5&result='.($_GET['result']).'" role="button" class="btn btn-secondary">Ne pas être rappelé</a>
-            <a href="" role="button" class="btn btn-secondary">Réinitialisation</a> 
-            <a href="" role="button" class="btn btn-secondary">Correction</a>
-                ';
+        <p>Votre numéro : '.$numero.'</p>
+        <a href="?question=5&result=<?= $_GET['result'] ?>" role="button" class="btn btn-success">Valider</a> <!-- Validation du numéro de téléphone -->
+        
+        <!-- Mes boutons-->
+        <br><br>
+        <a href="?question=5&result=<?= $_GET['result'] ?>" role="button" class="btn btn-secondary">Ne pas être rappelé</a>
+        <a href="" role="button" class="btn btn-secondary">Réinitialisation</a> 
+        <a href="" role="button" class="btn btn-secondary">Correction</a>
+        <?php 
             }
         ?>
           
-            <!-- Etape finale : A afficher si "oui" a été répondu à la question 3 ou si l'étape 4 a été résolue -->
+        <!-- Etape finale : A afficher si "oui" a été répondu à la question 3 ou si l'étape 4 a été résolue -->
             
         <?php       
             if(isset($_GET['question']) && $_GET['question'] == 5){ 
@@ -126,19 +121,17 @@
                     } else {
                         $note = "⚫";
                     }
-                    $ratings = $ratings . $note;
-                }
+                    $ratings .= $note;
+                }      
+        ?>             
                 
-                
-                echo '
-                <p class="mt-5">Merci pour votre notation : '. $ratings .' </p> <!-- le nombre d\'étoiles représente le nombre de points cumulés -->
+            <p class="mt-5">Merci pour votre notation : <?= $ratings ?> </p> <!-- le nombre d\'étoiles représente le nombre de points cumulés -->
 
-                <!-- Si un téléphone à été saisi, afficher "Vous serez rappelé très prochainement au #numéro de téléphone#" -->
-
-                <p class="mt-5">
-                    <a href="?" role="button" class="btn btn-danger">Recommencer</a>
-                </p>
-                ';
+            <!-- Si un téléphone à été saisi, afficher "Vous serez rappelé très prochainement au #numéro de téléphone#" -->
+            <p class="mt-5">
+                <a href="?" role="button" class="btn btn-danger">Recommencer</a>
+            </p>
+        <?php 
             }
         ?>
         
